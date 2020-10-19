8da0f157393683fb52c0a3a5cd48cc4cc8201662
williamburger6 <69281194+williamburger6@users.noreply.github.com>
2020-10-18 20:54:30 -0400

    Add files via upload

---
+++ willreviewssitefullstack/src/main/java/org/willwcci/willreviewssitefullstack/WillreviewssitefullstackApplication.java
@@ -0,0 +1,13 @@
+package org.willwcci.willreviewssitefullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WillreviewssitefullstackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WillreviewssitefullstackApplication.class, args);
+    }
+
+}
